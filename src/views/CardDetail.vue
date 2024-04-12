35b9e07e20364a93dec542439e491e2dcec8cc6c
Tigran Dzhamalyan <tigr.sa2021@gmail.com>
2024-04-12 16:39:57 +0300

    upd: detail page (draft)

--- src/views/CardDetail.vue
+++ src/views/CardDetail.vue
@@ -15,22 +15,25 @@ defineProps({
 		<div class="detail__carousel">
 			<UiSlider />
 		</div>
-		<div class="detail__upper">
-			<div class="detail__name">1-к. квартира, 43 м², 19/24 эт.</div>
-			<div class="detail__price">60_000 руб/мес</div>
+		<div class="detail__data">
+			<div class="detail__upper">
+				<h3 class="detail__subtitle">1-к. квартира, 43 м², 19/24 эт.</h3>
+				<h3 class="detail__subtitle">60_000 руб/мес</h3>
+			</div>
+			<h3 class="detail__subtitle">О квартире</h3>
+			<div class="detail__description">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, voluptate
+			</div>
+			<h3 class="detail__subtitle">Дата оплаты</h3>
+			<div class="detail__description">
+				Каждое 4 число месяца
+			</div>
+			<h3 class="detail__subtitle">Статус оплаты</h3>
+			<div class="detail__description">
+				Оплачено
+			</div>
 		</div>
-		<h3 class="detail__subtitle">О квартире</h3>
-		<div class="detail__description">
-			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, voluptate
-		</div>
-		<h3 class="detail__subtitle">Дата оплаты</h3>
-		<div class="detail__description">
-			Каждое 4 число месяца
-		</div>
-		<h3 class="detail__subtitle">Статус оплаты</h3>
-		<div class="detail__description">
-			Оплачено
-		</div>
+		<!-- /.detail__data -->
 	</div>
 </template>
 
@@ -39,10 +42,25 @@ defineProps({
 	max-height: 100vh;
 	padding: 0 2rem;
 
+	&__data {
+		width: fit-content;
+		margin: 0 auto;
+		padding: 2rem;
+
+		background-color: #f1f1f1;
+		border-radius: 1rem;
+		box-shadow: 12px 12px 2px 1px rgb(105, 166, 131);	}
+
 	&__title {
 		font-size: 1.25rem;
 		line-height: 1.5;
 		font-weight: 700;
+	}
+
+	&__subtitle {
+		font-size: 1.2rem;
+		line-height: 1.5;
+		font-weight: 600;
 	}
 
 	&__carousel {
@@ -50,7 +68,6 @@ defineProps({
 	}
 
 	&__upper {
-		width: 1000px;
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
