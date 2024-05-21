908f48c6358701f319d17bcc47302d93363310ec
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-05-21 18:15:06 +0300

    add: handle img while creating apartment card (draft)

--- src/views/CardCreate.vue
+++ src/views/CardCreate.vue
@@ -1,31 +1,136 @@
 <script setup>
+import { ref } from "vue";
+import { useAuthStore } from "@/store/authStore.js";
+import { storeToRefs } from "pinia";
+import VueJwtDecode from 'vue-jwt-decode';
+import { useRouter } from "vue-router";
+import axios from "axios";
+import config from "@/config/index.js";
+
+
+const router = useRouter();
+
+const authStore = useAuthStore();
+const { token } = storeToRefs((authStore));
+
+const decodedUserToken = VueJwtDecode.decode(token.value);
+
+const cardData = ref({
+	title: "",
+	description: "",
+	address: "",
+	square: null,
+	"room_count": null,
+	cost: null,
+	img: null,
+	user_id: decodedUserToken.id,
+});
+
+const handleImageUpload = (event) => {
+	const file = event.target.files[0];
+	if (file) {
+		cardData.value.img = file;
+	}
+};
+
+const createCard = async (data) => {
+	try	{
+		console.log("Creating card with data:", data);
+
+		const formData = new FormData();
+		formData.append('title', data.title);
+		formData.append('description', data.description);
+		formData.append('address', data.address);
+		formData.append('square', data.square);
+		formData.append('room_count', data.room_count);
+		formData.append('cost', data.cost);
+		formData.append('user_id', data.user_id);
+		formData.append('img', data.img);  // Append the image file
+
+		await axios.post('/apartment/create', formData, {
+			baseURL: config.apiBaseURI,
+			headers: {
+				'Content-Type': 'multipart/form-data'
+			}
+		});
+
+		await router.push("/");
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 </script>
 
 <template>
 	<div class="create">
-		<form action="">
+		<form @submit.prevent="createCard(cardData)" @keydown.enter="createCard(cardData)">
 			<h3>Создание апартамента</h3>
 			<div class="form__control">
-				<input class="form__input" type="text" placeholder="1-к. квартира, 43 м², 19/24 эт."/>
+				<input
+					v-model="cardData.title"
+					class="form__input"
+					type="text"
+					placeholder="1-к. квартира, 43 м², 19/24 эт."
+				/>
 			</div>
 			<div class="form__control">
-				<input class="form__input" type="text" placeholder="Описание"/>
+				<input
+					v-model="cardData.description"
+					class="form__input"
+					type="text"
+					placeholder="Описание"
+				/>
 			</div>
 			<div class="form__control">
-				<input class="form__input" type="text" placeholder="Адрес"/>
+				<input
+					v-model="cardData.address"
+					class="form__input"
+					type="text"
+					placeholder="Адрес"
+				/>
 			</div>
 			<div class="form__control">
-				<input class="form__input" type="number" placeholder="Площадь в кв.м"/>
+				<input
+					v-model="cardData.square"
+					class="form__input"
+					type="number"
+					placeholder="Площадь в кв.м"
+				/>
 			</div>
 			<div class="form__control">
-				<input class="form__input" type="number" placeholder="Кол-во комнат"/>
+				<input
+					v-model="cardData.room_count"
+					class="form__input"
+					type="number"
+					placeholder="Кол-во комнат"
+				/>
 			</div>
 			<div class="form__control">
-				<input class="form__input" type="number" placeholder="Стоимость в месяц"/>
+				<input
+					v-model="cardData.cost"
+					class="form__input"
+					type="number"
+					placeholder="Арендная плата"
+				/>
+			</div>
+			<div class="form__control">
+				<input
+					class="form__input"
+					type="file"
+					@change="handleImageUpload"
+				/>
 			</div>
 			<button class="form__btn">Создать</button>
 		</form>
+		<div v-if="cardData.img" class="preview">
+			<h4>Предварительный просмотр изображения:</h4>
+			<img
+				:src="cardData.img"
+				alt="Preview"
+				class="preview__image"
+			/>
+		</div>
 	</div>
 </template>
 
@@ -92,6 +197,18 @@ $sidebar-item-active: #276749;
 		&:focus {
 			background-color: $sidebar-item-hover;
 		}
+	}
+}
+
+.preview {
+	margin-top: 2rem;
+
+	&__image {
+		max-width: 50%;
+		height: auto;
+		border: 1px solid #e0e0e0;
+		border-radius: 8px;
+		box-shadow: 0 4px 17px #ccc;
 	}
 }
 </style>
