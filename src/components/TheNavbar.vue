d7f2459b3999467003e0400db3947db3384459d2
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-06-08 19:05:22 +0300

    add: navbar (draft: no-style)

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -20,5 +20,19 @@
 </template>
 
 <style scoped lang="scss">
-
+.navbar {
+	display: flex;
+	padding: 0 2rem;
+	justify-content: space-between;
+	align-items: center;
+	position: fixed;
+	z-index: 20;
+	top: 0;
+	left: 0;
+	right: 0;
+	height: 56px;
+	background-color: #2f855a;
+	box-sizing: border-box;
+	border-bottom: 1px solid #eaecef;
+}
 </style>
