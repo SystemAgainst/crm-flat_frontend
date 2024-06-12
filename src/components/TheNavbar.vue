af2c862b431309a3c5118c3d6d0402ef2713b6d8
Tigran Dzhamalyan <tigr.SA2021@gmail.com>
2024-06-12 08:45:37 +0300

    fix: style for navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -13,7 +13,7 @@ const store = useAuthStore();
 
 		<ul class="navbar-menu">
 			<li>
-				<router-link to="/">Главная</router-link>
+				<router-link class="navbar__link" to="/">Главная</router-link>
 			</li>
 			<li>
 				<router-link to="/docs">Документы</router-link>
@@ -26,6 +26,8 @@ const store = useAuthStore();
 </template>
 
 <style scoped lang="scss">
+$sidebar-bg-color: #2f855a;
+
 .navbar {
 	display: flex;
 	padding: 0 2rem;
@@ -37,8 +39,27 @@ const store = useAuthStore();
 	left: 0;
 	right: 0;
 	height: 56px;
-	background-color: #2f855a;
+	background-color: $sidebar-bg-color;
+	color: white;
 	box-sizing: border-box;
 	border-bottom: 1px solid #eaecef;
+}
+
+.navbar-menu {
+	list-style: none;
+	display: flex;
+}
+
+.navbar-menu li {
+	margin-right: 2rem;
+}
+
+.navbar-menu li a {
+	color: white;
+	text-decoration: none;
+}
+
+.navbar-menu li a:hover, .navbar-menu li a.active {
+	border-bottom: 2px solid #3eaf7c;
 }
 </style>
